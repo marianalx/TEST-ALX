6ac45665be0fa5494e74ccc2f738b4c3b6b1d71a
Marian <marianwitkowski@gmail.com>
2020-04-18 15:19:19 +0200

    initial commit

---
+++ Dziedziczenie/Pojazdy/Samochod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dziedziczenie.Samochody
+{
+    class Samochod : Pojazd
+    {
+        public void Uruchom()
+        {
+            Console.WriteLine("silnik start");
+        }
+
+        public void Zatankuj()
+        {
+            Console.WriteLine("Zalej benzyną/ON/LPG");
+        }
+
+        public void Zatrzymaj()
+        {
+            Console.WriteLine("silnik stop");
+        }
+    }
+}
